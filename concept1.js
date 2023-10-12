477c55a4cefb4a840bc61a18e0f06ad5a04bd2a8
Gagan <gagank2199@gmail.com>
2023-10-12 20:32:42 +0530

    af

--- concept1.js
+++ concept1.js
@@ -1,1 +1,11 @@
 document.body.style.backgroundColor='grey';
+
+// arrow function
+
+
+document.body.addEventListener('click',()=>
+{
+    document.body.style.backgroundColor='black';
+    document.body.style.color='white';
+})
+
