477ac51694b9190ddf867d8166cc4b1297c1f441
Gagan <gagank2199@gmail.com>
2023-10-14 20:37:22 +0530

    concept 2-b

--- concept1.js
+++ concept1.js
@@ -2,13 +2,10 @@ document.body.style.backgroundColor='grey';
 let con1=document.getElementById('con1');
 
 
-// let h4tag=document.getElementsByTagName('h4');
-
-
 // arrow function
 
 
-document.con1.addEventListener('click',()=>
+con1.addEventListener('click',()=>
 {
     // document.body.innerText='click event fired';
     document.body.style.backgroundColor='black';
--- concept2.js
+++ concept2.js
@@ -1,8 +1,11 @@
 let btn=document.getElementById('btn');
 let container=document.getElementsByClassName('container');
 
+// let container=document.querySelector('.container');                 this can also be use to access class name
+
 btn.addEventListener('click',()=>
 {
-    document.container.style.alignContent='center';
-    document.container.style.justifyContent='center';
+    // document.body.style.backgroundColor='red';
+    btn.style.color='blue';
+    container[0].style.backgroundColor='red';
 })
