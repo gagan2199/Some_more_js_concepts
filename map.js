00d0edff7e9b6827c4958e78835c78f126971a4a
Gagan <gagank2199@gmail.com>
2023-10-20 19:38:59 +0530

    map -p

--- map.js
+++ map.js
@@ -1,9 +1,15 @@
 let m=[1,2,3,34,53,22];
+let mm=[1,4,5,6,1,2,3,34,53,22];
 
 
 let nm=m.map((element)=>
 {
     return element+1;
 });
+let nmm=mm.map((element)=>
+{
+    return element+1;
+});
 
 console.log(nm);
+console.log(nmm);
