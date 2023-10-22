1848bc960b8618528cc7ac8ed2a70b72dc5d2ce0
Gagan <gagank2199@gmail.com>
2023-10-22 18:21:54 +0530

    sorting of numerical values

--- concept3.js
+++ concept3.js
@@ -1,7 +1,7 @@
 let var1=3;
 let arr1=[1,2,3,44,56,67,86,"ram","sham","akaash"];
 
-let numarr=[37,32,13,54,25,2,453,32,53];
+let numarr=[37,32,13,54,25,2,453,32,53];      // sorting of numerical values
 
 let srt1 = arr1.sort();
 
