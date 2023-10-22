c4489c05350d5d9678fb7767e4f6cfe95fb9beb4
Gagan <gagank2199@gmail.com>
2023-10-22 22:22:00 +0530

    string with numbers

--- concept3.js
+++ concept3.js
@@ -3,7 +3,7 @@ let arr1=[1,2,3,44,56,67,86,"ram","sham","akaash"];
 
 let numarr=[37,32,13,54,25,2,453,32,53];      // sorting of numerical values
 
-let srt1 = arr1.sort();
+let srt1 = arr1.sort();                 // sorting as dictionary
 
 let srt2= numarr.sort((a,b)=>
 {
