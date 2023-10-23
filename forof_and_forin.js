ec1541ba9ca08e31e63d0468b3dde3249c9c1aa8
Gagan <gagank2199@gmail.com>
2023-10-23 11:27:47 +0530

    for in added

--- forof_and_forin.js
+++ forof_and_forin.js
@@ -1,7 +1,13 @@
 let ab=[1,2,3,4,5,6];
 
 
-for (let i of ab)               // will print elements of ab, treat i as each element
+for (let item of ab)               // will print elements of ab, treat i as each element
+{
+    console.log(item);
+}
+
+
+for (let i in ab)               // will print element's key (0,1,2,3,4,5) of ab, treat i as key of each element
 {
     console.log(i);
 }
