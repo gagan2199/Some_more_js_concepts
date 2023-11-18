bf30f3a6f0b1f923ddf3c0696a03d21749dec4ab
Gagan <gagank2199@gmail.com>
2023-11-18 19:40:08 +0530

    reduce element added

--- reduce.js
+++ reduce.js
@@ -2,7 +2,7 @@ let colours=["red","blue","pink","grey","white","black","yellow"];
 
 let rc= colours.reduce((nam,nam2)=>                  // will return single value
 {
-    return nam+" " +nam2;
+    return nam+" "+nam2;
 })
 
 console.log(rc);
