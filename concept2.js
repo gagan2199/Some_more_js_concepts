a4c201ce6e0bb9f3f1edaab1b60157da54f1c432
Gagan <gagank2199@gmail.com>
2023-11-23 12:42:44 +0530

    SPL element added

--- concept2.js
+++ concept2.js
@@ -15,7 +15,7 @@ btn.addEventListener('click',()=>
 let a=[1,2,3,4,5,6];
 let c=[1,2,3,4,5,9,4,6];
 
-spl=a.splice(2,3,9,9,7);      // from index 2 3 elements 9,9,7 added
+spl=a.splice(2,3,9,122,7);      // from index 2 3 elements 9,122,7 added
 console.log(spl); 
 console.log(a); 
 
