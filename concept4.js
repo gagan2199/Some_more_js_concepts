784e88bfac18ae323f6113efcab4c9ec623766bb
Gagan Kumar <114229570+gagan2199@users.noreply.github.com>
2023-12-01 15:50:06 +0530

    adding reverse string code

--- concept4.js
+++ concept4.js
@@ -2,3 +2,21 @@ let body= document.getElementsByTagName('body');
 
 body.color='red';
 body.backgroundcolor='black';
+
+// program to reverse a string
+
+function reverseString(str) {
+
+    // empty string
+    let newString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+
+// take input from the user
+const string = prompt('Enter a string: ');
+
+const result = reverseString(string);
+console.log(result);
