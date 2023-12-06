55c3838b24482665139436d3c21a86454e49b535
Gagan Kumar <114229570+gagan2199@users.noreply.github.com>
2023-12-06 14:04:24 +0530

    Update concept4.js adding sorting

--- concept4.js
+++ concept4.js
@@ -20,3 +20,17 @@ const string = prompt('Enter a string: ');
 
 const result = reverseString(string);
 console.log(result);
+
+// order an array of names
+names.sort();
+
+// order an array of objects with name
+users.sort(function (a, b) {
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return 0;
+});
