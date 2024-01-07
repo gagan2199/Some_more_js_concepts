6b5353e8177d426dd835bd46cc0afa5eac37cb39
Gagan Kumar <114229570+gagan2199@users.noreply.github.com>
2024-01-07 16:12:18 +0530

    Update concept3.js

--- concept3.js
+++ concept3.js
@@ -3,7 +3,8 @@ let arr1=[1,2,3,44,56,67,86,"ram","sham","akaash","harsh"];
 
 let numarr=[37,32,13,54,25,2,453,32,53];      // sorting of numerical values
 
-let srt1 = arr1.sort();                 // sorting as dictionary
+let srt1 = arr1.sort();                
+ // sorting as dictionary
 
 let srt2= numarr.sort((a,b)=>
 {
@@ -13,9 +14,9 @@ return a-b;
 console.log(srt1);
 console.log(srt2);
 
-var a=["ram",28,"btech"]
+var a=["ram",23,"btech"]
         document.write(a +" "+"<br>");
-        a[1]=1;
+        a[1]=2;
         delete a[2];
         document.write(a +" "+"<br>");
    
